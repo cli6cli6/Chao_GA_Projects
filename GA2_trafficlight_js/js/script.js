f7bf3008b88dcc7ac989f4c46845ca01014c2039
cli6cli6 <cli6cli6@gmail.com>
2014-09-26 14:05:28 -0400

    trying it in js
    cloned files changed some html

---
+++ GA2_trafficlight_js/js/script.js
@@ -0,0 +1,8 @@
+function click(id) { 
+    $('#red,#yellow,#green').removeClass('active');
+    $(id).addClass('active');
+}
+
+$('#red_button').click(function(){click('#red')});
+$('#yellow_button').click(function(){click('#yellow')});
+$('#green_button').click(function(){click('#green')});
