e6f42e0b4979d5479a82fbc26aea7c409c765b13
ZackDinerstein <zackdhome@gmail.com>
2014-10-02 19:45:26 -0700

    Assignment #5 - Slideshow - Feedback

--- photoslide_project_chao/js/script.js
+++ photoslide_project_chao/js/script.js
@@ -1,27 +1,26 @@
 var catPics = ["img/cat0.png", "img/cat1.png", "img/cat2.png", "img/cat3.png", "img/cat4.png"];
-var currentImage = -4;
+var currentImage = 0; // Probably better to make this value 0 to start off with
 
 function changePicture(whichCat) {
 	currentImage = whichCat;
 	
 	var x = document.getElementById('mainImage');
 	x.setAttribute('src', catPics[currentImage]);
-
 }
 
 function nextImage() {
 	currentImage++;
 	
-	if(currentImage >= catPics.length) {
+	if (currentImage >= catPics.length) {
 		currentImage = 0;
 	}
 	
 	changePicture(currentImage);
 }
 
-function prevImage () {
+function prevImage() {
 	currentImage = currentImage - 1;
-	if(currentImage < 0) {
+	if (currentImage < 0) {
 		currentImage = catPics.length - 1;
 	}
 	changePicture(currentImage);
