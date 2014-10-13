e3579132d5ec94dbe4a748224b9698ea06f86316
ZackDinerstein <zackdhome@gmail.com>
2014-10-13 16:40:39 -0700

    Assignment #6 - Rock Paper Scissors - Feedback

--- rockpaperscissors/js/script.js
+++ rockpaperscissors/js/script.js
@@ -1,24 +1,33 @@
 var choice = ["rock", "paper", "scissors"];
 
 function evaluateChoices (playerChoice, cpuChoice) {
-console.log("Computer Choice: "+cpuChoice+", "+"Player Choice: "+ playerChoice);
-if (playerChoice === "rock"){
-	if(cpuChoice === "paper"){return 2;}
-		//0=draw, 1=player wins, 2= player loses
-	if(cpuChoice === "scissors"){ return 1;}
-	if(cpuChoice === "rock"){return 0;}
+  console.log("Computer Choice: " + cpuChoice + ", " + "Player Choice: " + playerChoice);
+  // Consider writing statement: 
+  // 
+  // if ( playerChoice == cpuChoice){
+  //     return 0;
+  // }
+  //
+  // Will cut down on amout of logic you need below.
+
+
+  if (playerChoice === "rock"){
+  	if(cpuChoice === "paper"){return 2;}
+  		//0=draw, 1=player wins, 2= player loses
+  	if(cpuChoice === "scissors"){ return 1;}
+  	if(cpuChoice === "rock"){return 0;}
 	}
 	
-if (playerChoice === "paper"){
-	if(cpuChoice === "paper"){return 0;}
-	if(cpuChoice === "scissors"){ return 2;}
-	if(cpuChoice === "rock"){return 1;}
-}
-if (playerChoice === "scissors"){
-	if(cpuChoice === "paper"){return 1;}
-	if(cpuChoice === "scissors"){ return 0;}
-	if(cpuChoice === "rock"){return 2;}
-}
+  if (playerChoice === "paper"){
+  	if(cpuChoice === "paper"){return 0;}
+  	if(cpuChoice === "scissors"){ return 2;}
+  	if(cpuChoice === "rock"){return 1;}
+  }
+  if (playerChoice === "scissors"){
+  	if(cpuChoice === "paper"){return 1;}
+  	if(cpuChoice === "scissors"){ return 0;}
+  	if(cpuChoice === "rock"){return 2;}
+  }
 }
 
 
@@ -26,36 +35,32 @@ $( document ).ready(function() {
     console.log( "ready!" );
     
   $("#submit").on("click",function(){
-  var variable = $("#choice").val();
-  
-  var cpuVariable = choice[Math.floor(Math.random()*choice.length)];
-  
-  if (variable === "random") {
-  	variable = choice[Math.floor(Math.random()*choice.length)];
-  }
-  
-  $("#results").text("Computer Choice: "+cpuVariable);
-  $("#resultstwo").text("Player Choice: "+ variable);
-  
-  evaluateChoices(variable, cpuVariable);
-  console.log(evaluateChoices(variable, cpuVariable));
-  switch (evaluateChoices(variable, cpuVariable)) {
-  	case 0: 
-  	$("#reveal").text("Tie")
-  	break;
-  	
-  	case 1: 
-  	$("#reveal").text("Player Wins!")
-  	break;
-  	
-  	case 2:
-  	$("#reveal").text("Player Loses")
-  	break;
-  }
-
+    var variable = $("#choice").val();
+    
+    var cpuVariable = choice[Math.floor(Math.random()*choice.length)];
+    
+    if (variable === "random") {
+    	variable = choice[Math.floor(Math.random()*choice.length)];
+    }
+    
+    $("#results").text("Computer Choice: " + cpuVariable);
+    $("#resultstwo").text("Player Choice: " + variable);
+    
+    evaluateChoices(variable, cpuVariable);
+    console.log(evaluateChoices(variable, cpuVariable));
+    switch (evaluateChoices(variable, cpuVariable)) {
+    	case 0: 
+      	$("#reveal").text("Tie")
+      	break;
+    	
+    	case 1: 
+      	$("#reveal").text("Player Wins!")
+      	break;
+    	
+    	case 2:
+      	$("#reveal").text("Player Loses")
+      	break;
+    }
   });
-  
-  
-  
 });
 
